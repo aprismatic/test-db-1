704dce52a7b41f4b23968279c3d2c1b9a06c1f7d
bazzilic <bazzilic@gmail.com>
2015-05-07 16:55:09 +0800

    Initial commit. Works with dedicated SQL Server and local development SQL Server engine (LocalDB) - Support for INSERT:     Only plaintext integer values - Support for SELECT:     Select expressions support constants, references to columns and addition

---
+++ Test-DB-1/Schema/Table1.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[col1.Paillier.Enc] VARBINARY(MAX) NULL, 
+	[col2.Paillier.Enc] VARBINARY(MAX) NULL,
+    [Common.Paillier.N] VARBINARY(MAX) NULL 
+)
---
+++ Test-DB-1/UDFs/PaillierAddition.cs
@@ -0,0 +1,16 @@
+using System.Data.SqlTypes;
+
+public partial class UserDefinedFunctions
+{
+    [Microsoft.SqlServer.Server.SqlFunction]
+    public static SqlBinary PaillierAddition(SqlBinary left, SqlBinary right, SqlBinary N)
+    {
+        var bi_left = new BigInteger(left.Value);
+        var bi_right = new BigInteger(right.Value);
+        var bi_N = new BigInteger(N.Value);
+
+        var bi_res = (bi_left * bi_right) % (bi_N * bi_N);
+        
+        return new SqlBinary(bi_res.getBytes());
+    }
+}
