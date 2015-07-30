004e6360d2418490c9e1c13d3c491b6c5d6b90a1
bazzilic <bazzilic@gmail.com>
2015-07-30 14:46:30 +0800

    InsertQuery now adds fingerprints when encrypts the data.

--- Test-DB-1/Schema/Table1.sql
+++ Test-DB-1/Schema/Table1.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[Table1]
 (
 	[col1.Paillier.Enc] VARBINARY(MAX) NULL, 
+	[col1.Fingerprint] INT NULL, 
 	[col2.Paillier.Enc] VARBINARY(MAX) NULL,
-    [Common.Paillier.N] VARBINARY(MAX) NULL 
+	[col2.Fingerprint] INT NULL ,
+    [Common.Paillier.N] VARBINARY(MAX) NULL
 )
